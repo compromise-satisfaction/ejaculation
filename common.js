2b0ce2a265c9e1172b05684813f9394eb3e35ed0
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2025-07-31 23:45:26 +0900

    Update common.js

--- common.js
+++ common.js
@@ -93,12 +93,12 @@ $(function(){
     orientationChange();
   });
 
-  /*
+  //*
   $("#base").on("touchstart",function(event){event.preventDefault();});
   $("#base").on("touchmove",function(event){event.preventDefault();});
   $("#base").on("touchend",function(event){event.preventDefault();});
   $("#base").on("touch",function(event){event.preventDefault();});
-  */
+  //*/
   //余白部分タッチの無効
 
   //ゲームを実行する
